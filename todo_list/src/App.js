54c07e9ecabf0054978ee68f0f1635eb8d45bd50
Bell, Ryan <ryan.bell62101@gmail.com>
2022-12-27 19:31:20 -0600

    Implemented adding and removing todo list items (exercize 2)

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,14 +3,55 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = React.useState([]);
   const [todo, setTodo] = React.useState("");
-return(
-<div className ="App">
-  <h1>Todo List</h1>
-  <form>
-    <input type ="text" align ="right" />
-    <button type ="submit">Add Todo</button>
-  </form>
-</div>
-);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+  
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+      setTodo("");
+    } else {
+      alert("Enter a valid task (must be at least 1 character)");
+      setTodo("");
+    }
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos].filter((item) => {
+      return item.id !== id;
+    })
+
+    setTodos(newTodos);
+  }
+
+  return(
+  <div className ="App">
+    <h1>Todo List</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        onChange={e => setTodo(e.target.value)} 
+        type ="text" align ="right" value={todo} />
+      <button type ="submit">Add Todo</button>
+    </form>
+    {todos.map((todoItem, index) => {
+      return (
+        <div 
+          className="todo"
+          key={todoItem.text + index}
+        >
+          {todoItem.text}
+          
+          <button onClick={() => removeTodo(todoItem.id)}>Delete</button>
+        </div>
+      )
+    })}
+  </div>
+  );
 };
 export default App;
